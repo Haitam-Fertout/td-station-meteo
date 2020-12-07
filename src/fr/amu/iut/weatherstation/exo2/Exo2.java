1bf66d6fd0d90d59c846ec763c6b4cd442b219e6
Antoine Merino <Merinorus@users.noreply.github.com>
2020-12-07 23:07:12 +0100

    TD Station Météo

---
+++ src/fr/amu/iut/weatherstation/exo2/Exo2.java
@@ -0,0 +1,8 @@
+package fr.amu.iut.weatherstation.exo2;
+
+public class Exo2 {
+    public static void main(String[] args) {
+        // TODO: Delete sample print and copy WeatherStation & Sensors from Exo1 to continue
+        System.out.println("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+    }
+}
